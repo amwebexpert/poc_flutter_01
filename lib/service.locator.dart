ece31754bce6dabcbfaf531472838d260ecba88f
André Masson <andre.masson@lacapitale.com>
2022-08-20 10:09:49 -0400

    chore: added initial structure

---
+++ lib/service.locator.dart
@@ -0,0 +1,14 @@
+import 'package:get_it/get_it.dart';
+
+import 'services/file/file.service.dart';
+import 'services/logger/logger.service.dart';
+
+final serviceLocator = GetIt.instance;
+
+Future<GetIt> initServiceLocator() async {
+  serviceLocator
+    ..registerLazySingleton<LoggerService>(() => LoggerService())
+    ..registerSingletonWithDependencies<FileService>(() => FileService(), dependsOn: [LoggerService]);
+
+  return serviceLocator;
+}
