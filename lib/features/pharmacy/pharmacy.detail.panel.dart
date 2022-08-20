7e98e4d8c3fde45d9225af3b768b74022b68e329
André Masson <andre.masson@lacapitale.com>
2022-08-20 14:24:47 -0400

    feat: pharmacy detail retrieval service

--- lib/features/pharmacy/pharmacy.detail.panel.dart
+++ lib/features/pharmacy/pharmacy.detail.panel.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:poc_flutter_01/services/pharmacies/pharmacy/pharmacy.address.model.dart';
 import 'package:poc_flutter_01/services/pharmacies/pharmacy/pharmacy.info.model.dart';
 
 import '../../services/pharmacies/pharmacy/pharmacy.model.dart';
@@ -13,20 +14,34 @@ class PharmacyDetailSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     PharmacyInfo info = pharmacy.value;
+    PharmacyAddress address = info.address;
+    List<String> hours = info.pharmacyHours.isBlank ? ['N/A'] : info.pharmacyHours.split(' \\n ');
+
     return Column(
       children: [
         const SizedBox(height: 20),
         Text('Location and contact information', style: Theme.of(context).textTheme.titleMedium),
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: CompactDatatableWidget(
-            rows: <DataRow>[
-              DataRow(cells: <DataCell>[const DataCell(Text('Name')), DataCell(Text(info.name))]),
-              DataRow(cells: <DataCell>[const DataCell(Text('Primary phone number')), DataCell(Text(info.primaryPhoneNumber))]),
-              DataRow(cells: <DataCell>[const DataCell(Text('Business hours')), DataCell(Text(info.pharmacyHours.isBlank ? 'N/A' : info.pharmacyHours))]),
+          child: Column(
+            children: [
+              CompactDatatableWidget(
+                rows: <DataRow>[
+                  DataRow(cells: <DataCell>[const DataCell(Text('Name')), DataCell(Text(info.name))]),
+                  DataRow(cells: <DataCell>[const DataCell(Text('Phone')), DataCell(Text(info.primaryPhoneNumber))]),
+                  DataRow(cells: <DataCell>[const DataCell(Text('Address')), DataCell(Text(address.streetAddress1))]),
+                  DataRow(cells: <DataCell>[const DataCell(Text('City')), DataCell(Text(address.city))]),
+                  DataRow(cells: <DataCell>[const DataCell(Text('Postal code')), DataCell(Text(address.postalCode))]),
+                  DataRow(cells: <DataCell>[const DataCell(Text('US Territory')), DataCell(Text(address.usTerritory))]),
+                ],
+              ),
             ],
           ),
         ),
+        const SizedBox(height: 24),
+        Text('Business hours', style: Theme.of(context).textTheme.titleMedium),
+        const SizedBox(height: 8),
+        ...hours.map((hour) => Text(hour)).toList(),
       ],
     );
   }
