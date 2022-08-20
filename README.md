e72cf7464a9081cf47131511e7a6af3e938323ac
André Masson <andre.masson@lacapitale.com>
2022-08-20 09:36:56 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# poc_flutter_01
+
+Flutter proof of concepts 01
+
+## Getting Started
+
---
+++ android/app/src/main/kotlin/com/example/poc_flutter_01/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.poc_flutter_01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
