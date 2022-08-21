8330a8fdebd0cfa3150166539836448f2ab15a84
André Masson <andre.masson@lacapitale.com>
2022-08-21 13:59:16 -0400

    feat: persist orders.json

--- lib/store/app.store.dart
+++ lib/store/app.store.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+
 import 'package:mobx/mobx.dart';
+import 'package:poc_flutter_01/services/file/directory.enum.dart';
+import 'package:poc_flutter_01/services/file/file.service.dart';
+
+import '../service.locator.dart';
 
 // Include generated file
 part 'app.store.g.dart';
@@ -8,12 +14,16 @@ class AppStore = AppStoreBase with _$AppStore;
 
 // The store-class
 abstract class AppStoreBase with Store {
+  final FileService fileService = serviceLocator.get<FileService>();
+
   @observable
   ObservableMap<String, List<String>> orders = ObservableMap.of({});
 
   @action
   void addOrder({required String pharmacyId, required List<String> medicaments}) {
     orders[pharmacyId] = medicaments;
+    String data = json.encode(orders);
+    fileService.write(data: data, filename: 'orders.json', directoryType: DirectoryType.appSupport);
   }
 
   bool hasOrderFor(String pharmacyId) => orders.containsKey(pharmacyId);
